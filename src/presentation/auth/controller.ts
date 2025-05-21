9ed2470033bcd8a6d5c7abd87e2805140937582c
Gabriel <gabrielpena0209@gmail.com>
2025-05-21 19:10:09 -0400

    first commit

---
+++ src/presentation/auth/controller.ts
@@ -0,0 +1,32 @@
+import { Request, Response } from 'express';
+import { RegisterUserDto } from '../../domain';
+import { AuthService } from '../services/auth.service';
+
+export class AuthController {
+
+    //DI
+    constructor(
+        public readonly authService: AuthService,
+    ) {}
+
+    registerUser = (req: Request, res: Response) => {
+
+        const [error, registerDto] = RegisterUserDto.create(req.body);
+
+        if (error) {
+            res.status(400).json({ error });
+            return;
+        }
+
+        this.authService.registerUser(registerDto!)
+            .then(user => res.json(user));
+    }
+
+    loginUser = (req: Request, res: Response) => {
+        res.json('loginUser')
+    }
+
+    validateEmail = (req: Request, res: Response) => {
+        res.json('validateEmail')
+    }
+}
