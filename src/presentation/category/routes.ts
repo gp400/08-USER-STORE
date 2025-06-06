0c7ba66d7238c293a32e149ec15845ffbc3d3904
= <=>
2025-06-06 11:35:39 -0400

    Categories

--- src/presentation/category/routes.ts
+++ src/presentation/category/routes.ts
@@ -1,13 +1,15 @@
 import { Router } from 'express';
 import { CategoryController } from './controller';
 import { AuthMiddleware } from '../middlewares/auth.middlware';
+import { CategoryService } from '../services/category.service';
 
 export class CategoryRoutes {
 
   static get routes(): Router {
 
     const router = Router();
-    const controller = new CategoryController();
+    const categoryService = new CategoryService();
+    const controller = new CategoryController(categoryService);
 
     router.get('/', controller.getCategories);
     router.post('/', [AuthMiddleware.validateJWT], controller.createCategory)
